72cfb923e5da0dd7679294fb31bebafe74fc73ec
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-11-01 21:25:15 -0400

    Primeiras aulas Ok

---
+++ aula001/script/aula02.js
@@ -0,0 +1,24 @@
+document.write('<h1>Olá Mundo!</h1>');
+console.log('Olá de novo, Mundo!');
+
+let mensagem = 'Minha mensagem';
+let mensagem2 = 'Outra mensagem';
+let meuPeso = 84.5;
+let minhaAltura = 1.85;
+let minhaIdade = 49;
+let ehDoador = false;
+let teste = null;
+
+const PI = 3.1415;
+const TAXA = 0.5;
+
+teste = 'Algum conteúdo'
+
+document.write('<p> Mensagem: ' + mensagem + '</p>');
+document.write(`Mensagem 2: ${mensagem2}`);
+document.write('<p> Meu peso: ' + meuPeso + '</p>');
+document.write(`Minha idade: ${minhaIdade}`);
+document.write('<p> Doador de orgãos: ' + ehDoador + '</p>');
+document.write(`Teste: ${teste}`);
+document.write('<p> PI:' + PI + '</p>');
+document.write(`TAXA: ${TAXA}`); 
